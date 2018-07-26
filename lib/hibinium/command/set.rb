c49b068516bc6605a700b2376383d434a98e11db
arikawa <arikawa@i3-systems.com>
2018-07-26 09:30:16 +0900

    fix - firefoxはちゃんとcloseしないとプロセスが残るっぽいのでcloseを入れた

--- lib/hibinium/command/set.rb
+++ lib/hibinium/command/set.rb
@@ -37,73 +37,81 @@ module Hibinium
       puts "user config file loaded!"
 
       # 日々報にログイン
-      browser     = chrome_hibifo
-      hibifo_page = Hibinium::Scenario.login_with(browser)
+      browser = chrome_hibifo
+      begin
+        hibifo_page = Hibinium::Scenario.login_with(browser)
 
-      # 指定の日に移動(指定なしの場合は不要)
-      hibifo_page.page_to_specified_date(specified_date.strftime("%Y-%m-%d")) unless date.empty?
-      puts "move to hibifo page"
+        # 指定の日に移動(指定なしの場合は不要)
+        hibifo_page.page_to_specified_date(specified_date.strftime("%Y-%m-%d")) unless date.empty?
+        puts "move to hibifo page"
 
-      # 入力済みでないか確認
-      #   入力済みだったら終わる
-      hibifo_page.report_edit_rows.each do |row|
-        if row.entered?
-          puts "warn: hibifo #{specified_date} is entered!!! stop input"
-          return
+        # 入力済みでないか確認
+        #   入力済みだったら終わる
+        hibifo_page.report_edit_rows.each do |row|
+          if row.entered?
+            puts "warn: hibifo #{specified_date} is entered!!! stop input"
+            return
+          end
         end
-      end
 
-      # 出勤/退勤打刻時間をCyberxeedから取得
-      checkin_out_row = get_checkin_out_row(specified_date)
-      unless checkin_out_row['出勤時刻'].rstrip.empty?
-        checkin_time = checkin_out_row['出勤時刻']
-        # 開始打刻に出勤打刻時刻をセット
-        hibifo_page.start_time = checkin_time
-        puts "勤務時間の開始時刻に#{checkin_time}を設定"
-      end
-      unless checkin_out_row['退勤時刻'].rstrip.empty?
-        checkout_time = checkin_out_row['退勤時刻']
-        # 終了打刻に退勤打刻時刻をセット
-        hibifo_page.end_time = checkout_time
-        puts "勤務時間の終了時刻に#{checkout_time}を設定"
-      end
+        # 出勤/退勤打刻時間をCyberxeedから取得
+        checkin_out_row = get_checkin_out_row(specified_date)
+        unless checkin_out_row['出勤時刻'].rstrip.empty?
+          checkin_time = checkin_out_row['出勤時刻'] == '__:__' ? '09:00' : checkin_out_row['出勤時刻']
+          # 開始打刻に出勤打刻時刻をセット
+          hibifo_page.start_time = checkin_time
+          puts "勤務時間の開始時刻に#{checkin_time}を設定"
+        end
+        unless checkin_out_row['退勤時刻'].rstrip.empty?
+          checkout_time = checkin_out_row['退勤時刻'] == '__:__' ? '18:00' : checkin_out_row['退勤時刻']
+          # 終了打刻に退勤打刻時刻をセット
+          hibifo_page.end_time = checkout_time
+          puts "勤務時間の終了時刻に#{checkout_time}を設定"
+        end
 
-      # テンプレートを入力
-      template.each_with_index do |job, i|
-        row          = hibifo_page.report_edit_rows[i]
-        row.job_code = job["code"]
-        row.job_text = job["text"]
-        row.job_time = job["time"]
-        puts "input row[#{i}] : #{job["code"]} | #{job["text"]} | #{job["time"]}"
-      end
+        # テンプレートを入力
+        template.each_with_index do |job, i|
+          row          = hibifo_page.report_edit_rows[i]
+          row.job_code = job["code"]
+          row.job_text = job["text"]
+          row.job_time = job["time"]
+          puts "input row[#{i}] : #{job["code"]} | #{job["text"]} | #{job["time"]}"
+        end
 
-      # 一時保存
-      hibifo_page.temporary_save
-      puts "hibifo #{specified_date} saved temporary"
+        # 一時保存
+        hibifo_page.temporary_save
+        puts "hibifo #{specified_date} saved temporary"
+      ensure
+        browser.close
+      end
     end
 
     private
 
     def get_checkin_out_row(specified_date)
-      browser  = firefox_cyberxeed
-      wwr_page = Hibinium::Scenario.login_and_move_to_wwr(browser)
+      browser = firefox_cyberxeed
+      begin
+        wwr_page = Hibinium::Scenario.login_and_move_to_wwr(browser)
 
-      # 指定の日を検索する
-      start_date            = Date.new(specified_date.year, specified_date.month, 1).to_s.gsub('-', '')
-      end_date              = specified_date.to_s.gsub('-', '')
-      wwr_page.period_start = start_date
-      wwr_page.period_end   = end_date
-      puts "#{start_date} ~ #{end_date}の期間で検索"
-      wwr_page.search
-      sleep 5 # TODO 高速化
-      result_table = wwr_page.result_table_hash
+        # 指定の日を検索する
+        start_date            = Date.new(specified_date.year, specified_date.month, 1).to_s.gsub('-', '')
+        end_date              = specified_date.to_s.gsub('-', '')
+        wwr_page.period_start = start_date
+        wwr_page.period_end   = end_date
+        puts "#{start_date} ~ #{end_date}の期間で検索"
+        wwr_page.search
+        sleep 5 # TODO 高速化
+        result_table = wwr_page.result_table_hash
 
-      date       = specified_date.strftime("%m/%d")
-      target_row = result_table.find do |row|
-        row['日付'] == date
+        date       = specified_date.strftime("%m/%d")
+        target_row = result_table.find do |row|
+          row['日付'] == date
+        end
+
+        return target_row
+      ensure
+        browser.close
       end
-
-      return target_row
     end
 
   end
